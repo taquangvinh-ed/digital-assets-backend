dc14be5a8007d18e7e07986cea021bfb2b5b1ecd
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2026-02-03 20:06:49 +0700

    feat: add application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,16 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:4.0.1")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:4.0.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/example/digital_assets/modules/auth/service/imp/AuthServiceImp.java
@@ -0,0 +1,8 @@
+package com.example.digital_assets.modules.auth.service.imp;
+
+import com.example.digital_assets.modules.auth.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImp implements AuthService {
+}
